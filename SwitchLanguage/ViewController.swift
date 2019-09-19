5e34b953a9302a0955ca8ef64234f7f5d8227fc0
Julio Collado <jcollado@altimetrik.com>
2019-09-19 11:26:54 -0300

    Initial Commit

---
+++ SwitchLanguage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwitchLanguage
+//
+//  Created by Julio Collado on 9/19/19.
+//  Copyright © 2019 Julio Collado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
